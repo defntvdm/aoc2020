6d2626e453274a2e0c7ddb8ce73217e85691005c
Vadim Nikolaev <defntvdm@gmail.com>
2020-12-08 21:39:21 +0500

    Day 8

--- src/d8.rs
+++ src/d8.rs
@@ -3,8 +3,34 @@ struct Command<'a> {
     n: i64,
 }
 
+fn execute(commands: &Vec<Command>) -> (i64, bool) {
+    let mut visited_commands: Vec<bool> = vec![false; commands.len()];
+    let mut current_op: i64 = 0;
+    let mut acc: i64 = 0;
+    while 0 <= current_op
+        && current_op < visited_commands.len() as i64
+        && !visited_commands[current_op as usize]
+    {
+        visited_commands[current_op as usize] = true;
+        match commands[current_op as usize].cmd {
+            "acc" => {
+                acc += commands[current_op as usize].n;
+                current_op += 1;
+            }
+            "nop" => {
+                current_op += 1;
+            }
+            "jmp" => {
+                current_op += commands[current_op as usize].n;
+            }
+            _ => {}
+        };
+    }
+    (acc, current_op == commands.len() as i64)
+}
+
 pub fn solve() {
-    let commands: Vec<Command> = include_str!("../input/d8.txt")
+    let mut commands: Vec<Command> = include_str!("../input/d8.txt")
         .lines()
         .map(|el| {
             let parts: Vec<&str> = el.trim().split(' ').collect();
@@ -14,24 +40,26 @@ pub fn solve() {
             }
         })
         .collect();
-    let mut visited_commands: Vec<bool> = vec![false; commands.len()];
-    let mut current_op: i64 = 0;
-    let mut acc: i64 = 0;
-    while 0 <= current_op && current_op < visited_commands.len() as i64 && !visited_commands[current_op as usize] {
-        visited_commands[current_op as usize] = true;
-        match commands[current_op as usize].cmd {
-            "acc" => {
-                acc += commands[current_op as usize].n;
-                current_op += 1;
-            },
-            "nop" => {
-                current_op += 1;
-            },
-            "jmp" => {
-                current_op += commands[current_op as usize].n;
-            },
-            _ => {},
-        };
-    }
+
+    let (acc, _) = execute(&commands);
+
+    // Star 1
     println!("{}", acc);
+
+    for i in 0..commands.len() {
+        if commands[i].cmd != "jmp" {
+            continue;
+        }
+
+        commands[i].cmd = "nop";
+
+        let (acc, full) = execute(&commands);
+        if full {
+            // Start 2
+            println!("{}", acc);
+            return;
+        }
+
+        commands[i].cmd = "jmp";
+    }
 }
