95a3438eaa7a86de24e0090e445d1c1d15091afa
Vadim Nikolaev <defntvdm@gmail.com>
2020-12-16 18:03:57 +0500

    Day 10 star 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -4,10 +4,11 @@ fn main() {
         .map(|a| a.parse::<i64>().unwrap())
         .collect();
     inp.sort();
+
     let mut one = 1;
     let mut three = 1;
     for i in 0..inp.len() - 1 {
-        match inp[i+1] - inp[i] {
+        match inp[i + 1] - inp[i] {
             1 => one += 1,
             3 => three += 1,
             _ => {}
@@ -15,4 +16,20 @@ fn main() {
     }
 
     println!("{}", one * three);
+
+    let mut dp: Vec<i128> = vec![0; inp.len()];
+    for i in 0i64..inp.len() as i64 {
+        if inp[i as usize] < 4 {
+            dp[i as usize] = 1;
+        }
+        for j in i - 3..i {
+            if j >= 0 {
+                if inp[i as usize] - inp[j as usize] < 4 {
+                    dp[i as usize] += dp[j as usize];
+                }
+            }
+        }
+    }
+
+    println!("{}", dp[dp.len() - 1]);
 }
